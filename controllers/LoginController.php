870432a3a6c44f91557b24979a0117e6f8f226cd
Djordy Decroix <67747764+Djordy59630@users.noreply.github.com>
2022-07-27 12:43:26 +0200

    fix login

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -39,11 +39,14 @@ class LoginController extends BaseController {
 				$user = $userLogin->userLogin($emailVerify);
 
 			
+
+
 				// Ici on a un user existant, on peut vérifier le mot de passe
 				if(!$user || !password_verify($request->get("password"), $user["pwd"]) ){
 					header('Location: /message/L\'utilisateur et/ou le mot de passe est incorrect'); 
 				}
 
+			
 				// L'utilisateur est le mot de passe sont corrects
 				
 	
